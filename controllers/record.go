374cc4b5917f09a0ee99d4e06a7cf4e5747902ec
jianghongbin <jianghongbin@kingsoft.com>
2018-11-30 17:41:56 +0800

    [feat]-vod test

--- controllers/record.go
+++ controllers/record.go
@@ -63,5 +63,7 @@ func vod (tsPath string, m3u8Path string, bucket string) {
 	vodName := cache.Get(bucket)
 	hdfs:= service.WebHdfsClient{}
 	remoteFile := path.Join(hdfsprefix, vodName, "index.m3u8")
+	log.Info("写入hdfs 路径", remoteFile)
 	hdfs.UploadFile(temp, remoteFile)
+	log.Info("写入hdfs 成功")
 }
